c386e1b0cb50861693e6ebdc62818de893c877d8
Ivannn69 <147012652+Ivannn69@users.noreply.github.com>
2023-10-05 13:57:13 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './assets/components/button'
+
+function App() {
+  const [estado, setEstado] = useState("Verdadero")
+
+  return (
+
+    <>
+      <div>
+        <h1> {estado} </h1>
+
+        <button 
+          onClick={()=>{
+            if (estado == "Verdadero") {
+              setEstado("Falso");
+            } else {
+              setEstado("Verdadero");
+            }
+          }}
+        >     
+          X
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App
